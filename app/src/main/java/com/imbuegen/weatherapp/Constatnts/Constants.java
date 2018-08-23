2bbaa6b5b8c35eb4a3ca44cedc79de277abcbb67
Yash Javeri <35690656+YashJaveri@users.noreply.github.com>
2018-08-24 00:45:50 +0530

    Update Constants.java

--- app/src/main/java/com/imbuegen/weatherapp/Constatnts/Constants.java
+++ app/src/main/java/com/imbuegen/weatherapp/Constatnts/Constants.java
@@ -1,7 +1,7 @@
 package com.imbuegen.weatherapp.Constatnts;
 
 public class Constants {
-    final static private String APP_ID = "df78dcfa9580e72b15fdf62d406d34ec";
+    final static private String APP_ID;    //add APP ID/API KEY
 
     static public String getAPP_ID() {
         return APP_ID;
